ae6ae7f93aed70534111b5335c0de531c1acdc27
Will <102735027+Wilian010102@users.noreply.github.com>
2025-06-18 14:39:10 +0700

    Update and change
    Update and change in PatientController, PatientRequestDTO, GlobalExceptionHandler, PatientMapper, and PatientService.

--- patient-service/src/main/java/com/pm/patient_service/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/pm/patient_service/mapper/PatientMapper.java
@@ -1,7 +1,10 @@
 package com.pm.patient_service.mapper;
 
+import com.pm.patient_service.dto.PatientRequestDTO;
 import com.pm.patient_service.dto.PatientResponseDTO;
 import com.pm.patient_service.model.Patient;
+
+import java.time.LocalDate;
 
 public class PatientMapper {
     public static PatientResponseDTO toDTO(Patient patient) {
@@ -11,5 +14,15 @@ public class PatientMapper {
         patientDTO.setAddress(patient.getAddress());
         patientDTO.setDateofBirth(patient.getDateOfBirth().toString());
         return patientDTO;
+    }
+
+    public static Patient toModel(PatientRequestDTO patientRequestDTO) {
+        Patient patient = new Patient();
+        patient.setName(patientRequestDTO.getName());
+        patient.setAddress(patientRequestDTO.getAddress());
+        patient.setEmail(patientRequestDTO.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestDTO.getDateOfBirth()));
+        patient.setRegisteredDate(LocalDate.parse(patientRequestDTO.getRegisteredDate()));
+        return patient;
     }
 }
