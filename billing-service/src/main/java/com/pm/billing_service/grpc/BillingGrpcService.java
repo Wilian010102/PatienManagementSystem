92011c0c8c51a3e5f7a801d0b8986d86d9f7d65d
Will <102735027+Wilian010102@users.noreply.github.com>
2025-06-25 20:53:09 +0700

    modif pom.xml and BillingGRPCService.java

--- billing-service/src/main/java/com/pm/billing_service/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/pm/billing_service/grpc/BillingGrpcService.java
@@ -5,11 +5,23 @@ import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import billing.BillingRequest;
 import billing.BillingResponse;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @GrpcService
 public class BillingGrpcService extends BillingServiceImplBase {
     @Override
     public void createBillingAccount(BillingRequest billingRequest, StreamObserver<BillingResponse> responseObserver) {
+        log.info("createBillingAccount request received {}", billingRequest.toString());
 
+        //Business logic - e.g save to database, perform, perform calculate etc
+
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 }
